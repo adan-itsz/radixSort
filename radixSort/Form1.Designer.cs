cdc5df7f279cd05a7f86623671ac0ece67961463
Adan De Santiago <adan1995a@gmail.com>
2015-11-10 23:35:54 -0600

    muestra iteracciones en DGV

---
+++ radixSort/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace radixSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxLongitud = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLlenar = new System.Windows.Forms.Button();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBoxLongitud
+            // 
+            this.txtBoxLongitud.Location = new System.Drawing.Point(26, 109);
+            this.txtBoxLongitud.Name = "txtBoxLongitud";
+            this.txtBoxLongitud.Size = new System.Drawing.Size(122, 22);
+            this.txtBoxLongitud.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "longitud de arreglo";
+            // 
+            // btnLlenar
+            // 
+            this.btnLlenar.Location = new System.Drawing.Point(26, 194);
+            this.btnLlenar.Name = "btnLlenar";
+            this.btnLlenar.Size = new System.Drawing.Size(122, 23);
+            this.btnLlenar.TabIndex = 2;
+            this.btnLlenar.Text = "Llenar";
+            this.btnLlenar.UseVisualStyleBackColor = true;
+            this.btnLlenar.Click += new System.EventHandler(this.btnLlenar_Click);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(26, 245);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(122, 23);
+            this.btnOrdenar.TabIndex = 3;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(28, 137);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(122, 23);
+            this.btnAceptar.TabIndex = 4;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(221, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(389, 271);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 315);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(389, 183);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 510);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.btnLlenar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxLongitud);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxLongitud;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLlenar;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ radixSort/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace radixSort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int longitud;
+        int[] array;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                longitud = Convert.ToInt32(txtBoxLongitud.Text);
+                array = new int[longitud];
+                dataGridView1.Columns.Add("", "arreglo original");
+                for (int i = 0; i < array.Length  - 1; i++)
+                {
+                    dataGridView1.Rows.Add();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("llene correctamente");
+            }
+        }
+
+        private void btnLlenar_Click(object sender, EventArgs e)
+        {
+            Random rando= new Random();
+            int num;
+            for (int i = 0; i < longitud; i++)
+            {
+                num=rando.Next(100,999);
+                array[i] = num;
+                dataGridView1[0, i].Value = num;
+            }
+        }
+
+        public void radixSort(int[]arreglo)
+        {
+            int numBytes = 3;
+            int cont;
+            int aux;
+            int contadorAcomodo=0;
+            int columna = 0;
+            Queue<int>[] cola = new Queue<int>[10];
+            for (int i = 0; i < 10; i++)
+            {
+                cola[i]= new Queue<int>();
+              //inicializar colas
+            }
+                for (int i = 1; i <= numBytes; i++)
+                {
+                    cont = 0;
+                    aux = 0;
+                    for (int j = 0; j < arreglo.Length; j++)
+                    {
+                        aux = arreglo[j];
+                        if (i == 1)
+                        {
+                            cont = aux % 10;
+                        }
+                        else  if (i == 2)
+                        {
+                            cont = aux / 10;
+                            cont = cont % 10;
+                        }
+                        else
+                        {
+                            cont = aux / 100;
+                        }
+                        cola[cont].Enqueue(aux);
+                    }
+                    
+
+                        for (int j = 0; j < 10; j++)
+                        {
+                            while (cola.Count() != 0)
+                            {
+                                if (cola[j].Count == 0)
+                                {
+                                    break;
+                                }
+                                arreglo[contadorAcomodo++] = cola[j].Dequeue();
+                            }
+
+                        }
+                    contadorAcomodo = 0;
+                    //mostar en el DGV
+                        dataGridView1.Columns.Add("", " ");
+                        columna++;
+                  
+                    for (int x = 0; x < arreglo.Length; x++)
+                    {
+                        dataGridView1[columna, x].Value = arreglo[x];
+                    }
+
+                }
+		        
+	        
+
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            radixSort(array);
+        }
+    }
+}
